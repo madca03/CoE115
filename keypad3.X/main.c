1c40d7e67346d79d70651859ffeb0ca432a276a5
Mark <markallenagaton03@gmail.com>
2017-02-27 12:39:18 +0800

    finish lab3 - detecting button presses in a column

--- keypad3.X/main.c
+++ keypad3.X/main.c
@@ -1,7 +1,7 @@
 /*
  * File:   main.c
  * Author: Mark
- *
+ * Description: Lab3 Seatwork - Detecting button presses in a column
  * Created on February 27, 2017, 4:03 AM
  */
 
@@ -14,10 +14,16 @@ _CONFIG3 (SOSCSEL_IO)
 #define DEB_MAX 10
 
 void __attribute__((interrupt)) _CNInterrupt(void);
+
 void led1_toggle(void);
 void led2_toggle(void);
+void led3_toggle(void);
+void led4_toggle(void);
+
 int row1_press;
 int row2_press;
+int row3_press;
+int row4_press;
 
 int main(void) {
     /* Configure ports
@@ -26,34 +32,38 @@ int main(void) {
      * RA2 - row 3 (input)
      * RA3 - row 4 (input)
      * RB0 - col 1 (output)
-     * RB1 - col 2 (output)
-     * RB2 - col 3 (output)
      * RB4 - led 1 (output)
      * RB5 - led 2 (output)
+     * RB7 - led 3 (output)
+     * RB8 - led 4 (output)
      */
     
     AD1PCFG = 0xffff;
-    TRISA = 0x0003;
-    TRISB = 0xffce;
+    TRISA = 0x000f;
+    TRISB = 0xfe4e;
     LATB = 0xffff;
     
     /* Enable internal pullups
      * RA0 - CN2 (CNPU1)
      * RA1 - CN3 (CNPU1)
+     * RA2 - bit 14 (CNPU2)
+     * RA3 - bit 13 (CNPU2)
      */
     CNPU1 = CNPU1 | 0x000c;
+    CNPU2 = CNPU2 | 0x6000;
     
     /* Enable interrupts and clear IRQ flag
      * RA0 - CN2 (CNEN1)
      * RA1 - CN3 (CNEN1)
+     * RA2 - bit 14 (CNEN2)
+     * RA3 - bit 13 (CNEN2)
      */
     CNEN1 = CNEN1 | 0x000c;
+    CNEN2 = CNEN2 | 0x6000;
     IEC1bits.CNIE = 1;
     IFS1bits.CNIF = 0;
     
     /* Pull down col1 - LATB0 
-     * col2 - LATB1
-     * col3 - LATB2
      */
     
     /* When all cols are pulled down -> all keys in a row toggle the led
@@ -62,8 +72,6 @@ int main(void) {
      */
     
     LATBbits.LATB0 = 1;
-    // LATBbits.LATB1 = 0;
-    // LATBbits.LATB2 = 0;
     while (1) {
         if (row1_press) {
             led1_toggle();
@@ -71,6 +79,12 @@ int main(void) {
         } else if (row2_press) {
             led2_toggle();
             row2_press = 0;
+        } else if (row3_press) {
+            led3_toggle();
+            row3_press = 0;
+        } else if (row4_press) {
+            led4_toggle();
+            row4_press;
         }
     }
     
@@ -97,6 +111,22 @@ void __attribute__((interrupt)) _CNInterrupt(void) {
             row2_press = 1;
         else
             row2_press = 0;
+    } else if (!PORTAbits.RA2) {
+        while ((!PORTAbits.RA2) && (deb_ctr < DEB_MAX)) {
+            deb_ctr++;
+        }
+        if (deb_ctr == DEB_MAX)
+            row3_press = 1;
+        else
+            row3_press = 0;
+    } else if (!PORTAbits.RA3) {
+        while ((!PORTAbits.RA3) && (deb_ctr < DEB_MAX)) {
+            deb_ctr++;
+        }
+        if (deb_ctr == DEB_MAX)
+            row4_press = 1;
+        else
+            row4_press = 0;
     }
     
     /* Clear IRQ flag */
@@ -109,4 +139,12 @@ void led1_toggle(void) {
 
 void led2_toggle(void) {
     LATBbits.LATB5 = ~LATBbits.LATB5;
+}
+
+void led3_toggle(void) {
+    LATBbits.LATB7 = ~LATBbits.LATB7;
+}
+
+void led4_toggle(void) {
+    LATBbits.LATB8 = ~LATBbits.LATB8;
 }
