bbaa4763082ea7059134d6a2756894a34a05f93c
Mark <markallenagaton03@gmail.com>
2017-03-17 09:12:12 +0800

    update milestone 2

--- me1/pic24fj64ga002/milestone2.X/main.c
+++ me1/pic24fj64ga002/milestone2.X/main.c
@@ -27,6 +27,7 @@
 
 #define CHAR_HEX_SIZE (4)
 #define LCD_WRITE_EXEC_TIME (100) 
+#define DEB_MAX (500)
 
 #define COMMAND (0x01)
 #define DATA    (0x02)
@@ -94,9 +95,28 @@ void LCD_print(char *data);
 void ADC_init();
 void __attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void);
 
+void __attribute__((interrupt)) _CNInterrupt(void);
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void);
+void keypad_init(void);
+void PWM_init(void);
+void PWM_pin_setup(void);
+
 int adcvalue;
 char adcvalue_str[17];
 int i = 0;
+
+int one_press;
+int two_press;
+int three_press;
+int four_press;
+int five_press;
+int six_press;
+int seven_press;
+int eight_press;
+int nine_press;
+int star_press;
+int zero_press;
+int hash_press;
 
 int main(void) {
   // RB14 - LCD enable pin
@@ -105,6 +125,9 @@ int main(void) {
 
   // RB15 - temp sensor analog pin
 
+  // RA[3:0] - keypad rows
+
+  TRISA = 0x000F;
   TRISB = 0x90FF;
 
   ADC_init();
@@ -114,7 +137,19 @@ int main(void) {
   LCD_set_cursor(ROW1, 0);
   LCD_print("Agaton");
 
+  PWM_pin_setup();
+  // PWM_init();
+  // keypad_init();
+
   while(1) {
+    // if (two_press) {
+    //   if (OC1RS <= PR2) OC1RS = OC1RS + 1;
+    //   two_press = 0;
+    // } else if (eight_press) {
+    //   if (OC1RS) OC1RS = OC1RS - 1;
+    //   eight_press = 0;
+    // }
+
     LCD_return_home();
     LCD_print(adcvalue_str);
   }
@@ -148,7 +183,7 @@ void __attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void) {
     IFS0bits.AD1IF = 0; // Clear interrupt
     
     adcvalue = ADC1BUF0;
-    sprintf(adcvalue_str, "%lf", ((double) adcvalue / pow(2,10)) * 3.43 * 100);
+    sprintf(adcvalue_str, "%lf", ((double) adcvalue / pow(2,10)) * 3.43 * 100 / 5);
 
     IEC0bits.AD1IE = 1; // Enable interrupt
     IFS0bits.AD1IF = 0; // Clear interrupt
@@ -221,4 +256,294 @@ void LCD_send4bit(data) {
   LATB = LATB | data_bits;
   __delay_us(1);
   LATB = 0;
+}
+
+void keypad_init(void) {
+    /* Enable internal pullups
+     * RA0 - CN2 (CNPU1)
+     * RA1 - CN3 (CNPU1)
+     * RA2 - bit 14 (CNPU2)
+     * RA3 - bit 13 (CNPU2)
+     */
+    CNPU1 = CNPU1 | 0x000c;
+    CNPU2 = CNPU2 | 0x6000;
+
+    /* Enable interrupts and clear IRQ flag
+     * RA0 - CN2 (CNEN1)
+     * RA1 - CN3 (CNEN1)
+     * RA2 - bit 14 (CNEN2)
+     * RA3 - bit 13 (CNEN2)
+     */
+    CNEN1 = CNEN1 | 0x000c;
+    CNEN2 = CNEN2 | 0x6000;
+    IEC1bits.CNIE = 1;
+    IFS1bits.CNIF = 0;
+
+    // Pull down all columns
+    LATBbits.LATB0 = 0;
+    LATBbits.LATB1 = 0;
+    LATBbits.LATB2 = 0;
+}
+
+void PWM_init(void) {
+    OC1CON = 0;
+    OC1R = 0xFFFF;
+    OC1RS = 0x004F;
+    OC1CON = 0x2000;
+    OC1CONbits.OCM = 0x6;
+    PR2 = 0x009F;
+    T2CON = 0;          // reset Timer 2 control register
+    
+    IFS0bits.T2IF = 0;  // clear the Timer 2 interrupt status flag
+    IEC0bits.T2IE = 1;  // Enable Timer 2 interrupt
+    T2CONbits.TON = 1;  // Start Timer 2 with prescaler settings at 1:1 and
+                        // clock source set to the internal instruction cycle
+}
+
+void PWM_pin_setup(void) {
+    __builtin_write_OSCCONL(OSCCON & (~(1<<6)));
+    RPOR3bits.RP7R = 0x12;  // set RP7 as output compare 1
+    __builtin_write_OSCCONL(OSCCON|(1<<6));
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
+    IFS0bits.T2IF = 0;  // Reset Timer2 interrupt flag and return from ISR
+}
+
+void __attribute__((interrupt)) _CNInterrupt(void) {
+    int deb_ctr = 0;    // debounce counter
+    int i = 0;
+    
+    if (!PORTAbits.RA0) {
+        for (i = 0; i < 3; i++) {
+            if (i == 0) {
+                LATBbits.LATB1 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA0) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    one_press = 1;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    one_press = 0;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 1) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA0) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    two_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    two_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 2) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB1 = 1;
+                
+                while ((!PORTAbits.RA0) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    three_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                    break;
+                } else {
+                    three_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                }
+            }
+        }
+    } else if (!PORTAbits.RA1) {
+        for (i = 0; i < 3; i++) {
+            if (i == 0) {
+                LATBbits.LATB1 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA1) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    four_press = 1;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    four_press = 0;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 1) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA1) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    five_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    five_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 2) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB1 = 1;
+                
+                while ((!PORTAbits.RA1) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    six_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                    break;
+                } else {
+                    six_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                }
+            }
+        }
+    } else if (!PORTAbits.RA2) {
+        for (i = 0; i < 3; i++) {
+            if (i == 0) {
+                LATBbits.LATB1 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA2) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    seven_press = 1;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    seven_press = 0;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 1) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA2) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    eight_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    eight_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 2) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB1 = 1;
+                
+                while ((!PORTAbits.RA2) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    nine_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                    break;
+                } else {
+                    nine_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                }
+            }
+        }
+    } else if (!PORTAbits.RA3) {
+        for (i = 0; i < 3; i++) {
+            if (i == 0) {
+                LATBbits.LATB1 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA3) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    star_press = 1;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    star_press = 0;
+                    LATBbits.LATB1 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 1) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB2 = 1;
+                
+                while ((!PORTAbits.RA3) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    zero_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                    break;
+                } else {
+                    zero_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB2 = 0;
+                }
+            }
+            else if (i == 2) {
+                LATBbits.LATB0 = 1;
+                LATBbits.LATB1 = 1;
+                
+                while ((!PORTAbits.RA3) && (deb_ctr < DEB_MAX)) {
+                    deb_ctr++;
+                }
+                if (deb_ctr == DEB_MAX) {
+                    hash_press = 1;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                    break;
+                } else {
+                    hash_press = 0;
+                    LATBbits.LATB0 = 0;
+                    LATBbits.LATB1 = 0;
+                }
+            }
+        }
+    }
+    
+    /* Clear IRQ flag */
+    IFS1bits.CNIF = 0;
 }
